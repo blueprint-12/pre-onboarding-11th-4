a16bd35ed7f7cfc9cba53dd2e3b4a7e30d08075a
blueprint-12 <blueprint12@daum.net>
2023-08-09 23:56:10 +0900

    feat: 검색시 사용할, 디바운스 커스텀 훅 생성

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+// delay default value 500ms
+const useDebounce = (value: string, delay = 500) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
