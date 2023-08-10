9b9f9e6ad75ce8f04da6bd2098af0a3966c346c0
blueprint-12 <blueprint12@daum.net>
2023-08-10 23:56:45 +0900

    feat: http client (axios)

---
+++ src/utils/api/customAxios.ts
@@ -0,0 +1,58 @@
+import axios, { AxiosInstance, AxiosError, AxiosResponse } from 'axios';
+
+interface CustomError {
+  status: number;
+  message: string;
+}
+
+interface CustomAxiosError extends AxiosError {
+  response?: AxiosResponse<CustomError>;
+}
+
+const BASE_URL = 'localhost:4000';
+
+export const api: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+//요청 API
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (err: CustomAxiosError) => {
+    if (err.response) {
+      const { status, data } = err.response;
+      const errResponse: CustomError = {
+        status,
+        message: data.message,
+      };
+      return Promise.reject(errResponse);
+    }
+    return Promise.reject(err);
+  },
+);
+
+//응답 API
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (err: CustomAxiosError) => {
+    if (err.response) {
+      const { status, data } = err.response;
+      const errResponse: CustomError = {
+        status,
+        message: data.message,
+      };
+      return Promise.reject(errResponse);
+    }
+    return Promise.reject(err);
+  },
+);
+
+export default api;
