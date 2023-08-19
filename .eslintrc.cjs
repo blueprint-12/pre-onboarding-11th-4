73a49119b6a5d1158a4f79166f810c48de2d775a
blueprint-12 <blueprint12@daum.net>
2023-08-19 23:51:04 +0900

    chore: eslint, prettier, husky(git hook), settings
    - eslint 세팅
    - prettier 세팅
    - husky 세팅
    - .vscode 추가 익스텐션 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,35 @@
 /* eslint-env node */
 
-module.exports =
-  {
-    root: true,
-    env: {
-      browser: true,
-      es2020: true,
-    },
-    extends:
-      [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:react-hooks/recommended',
-      ],
-    parser:
-      '@typescript-eslint/parser',
-    parserOptions:
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
       {
-        ecmaVersion:
-          'latest',
-        sourceType:
-          'module',
-        project: true,
-        tsconfigRootDir:
-          __dirname,
+        allowConstantExport: true,
       },
-    plugins:
-      [
-        'react-refresh',
-      ],
-    rules:
-      {
-        'react-refresh/only-export-components':
-          [
-            'warn',
-            {
-              allowConstantExport: true,
-            },
-          ],
-        '@typescript-eslint/no-non-null-assertion':
-          'off',
-      },
-     
-  };
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+};
